3a8b5ce3f3b4f846d47e32a97b57ded7fd3ba4a0
Shivam Mishra <mshivam2062@gmail.com>
2026-02-05 14:13:38 +0530

    A URL Shortener web application built using Node.js, Express, MongoDB, and Mongoose. It generates short links, tracks visit history, and redirects users to original URLs.

---
+++ controllers/url.controller.js
@@ -0,0 +1,24 @@
+const { nanoid } = require('nanoid');
+const URL = require("../models/url.js");
+
+async function handlegrenratenewurl(req, res) {
+    const body = req.body;
+
+    if (!body.url) {
+        return res.status(400).json({ error: "Url is required" });
+    }
+
+    const shortId = nanoid(8);
+
+    await URL.create({
+        shortId: shortId,
+        redirectUrl: body.url,
+        visitHistory: [],
+    });
+
+    return res.json({ id: shortId });
+}
+
+module.exports = {
+    handlegrenratenewurl,
+};
